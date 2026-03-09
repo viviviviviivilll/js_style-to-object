265e345b1cec4b752ae96ffa84658e69a88c7d9a
viviviviviivilll <bbojcuk44@gmail.com>
2026-03-09 02:25:02 +0200

    #1 version

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const finalCssObject = {};
+  const TEXT_SPLIT_OPERATOR = ';';
+  const LINE_SPLIT_OPERATOR = ':';
+  const PROPERTY_KEY_INDEX = 0;
+  const PROPERTY_VALUE_INDEX = 1;
+
+  sourceString.split(TEXT_SPLIT_OPERATOR).map((line) => {
+    const lineParts = line.split(LINE_SPLIT_OPERATOR);
+    const keyPart = lineParts[PROPERTY_KEY_INDEX]?.trim();
+    const valuePart = lineParts[PROPERTY_VALUE_INDEX]?.trim();
+
+    finalCssObject[keyPart] = valuePart;
+  });
+
+  return finalCssObject;
 }
 
 module.exports = convertToObject;
